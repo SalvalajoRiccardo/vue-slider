42ad2984ca827a27e20e71bc60c648ba1eb64d9e
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-06-22 15:43:24 +0200

    avanzamebto 1

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    
+    el: '#root',
+    data: {
+        images: ['img\carolus-rex.jpg','img\coat-of-arms.jpg', 'img\heroes.jpg', 'img\the-great-war.jpg', 'img\the-last-stand.jpg']
+    },
+    methods: {
+
+    }
+});
