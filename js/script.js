09f1f58f8acd710082a63db9f93f453e237d6a47
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-06-22 16:31:17 +0200

    fine

--- js/script.js
+++ js/script.js
@@ -2,9 +2,24 @@ var app = new Vue({
     
     el: '#root',
     data: {
-        images: ['img\carolus-rex.jpg','img\coat-of-arms.jpg', 'img\heroes.jpg', 'img\the-great-war.jpg', 'img\the-last-stand.jpg']
+        contatore: 0,
+        images: ['img/carolus-rex.jpg','img/coat-of-arms.jpg', 'img/heroes.jpg', 'img/the-great-war.jpg', 'img/the-last-stand.jpg']
     },
     methods: {
-
+        nextAlbum() {
+            
+            if (this.contatore == this.images.length - 1){
+                this.contatore = 0;
+            } else {this.contatore++;}
+        },
+        prevAlbum() {
+            
+            if (this.contatore == 0){
+                this.contatore = this.images.length - 1;
+            } else {this.contatore--;}
+        },
+        change(i) {
+            this.contatore = i;
+        }
     }
 });
