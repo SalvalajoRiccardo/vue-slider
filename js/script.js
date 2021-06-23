fcb210c641fde3e5464a07f9a02d261cf7c63272
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-06-23 17:21:06 +0200

    add bottoni start and stop

--- js/script.js
+++ js/script.js
@@ -3,28 +3,40 @@ var app = new Vue({
     el: '#root',
     data: {
         contatore: 0,
-        images: ['img/carolus-rex.jpg','img/coat-of-arms.jpg', 'img/heroes.jpg', 'img/the-great-war.jpg', 'img/the-last-stand.jpg']
+        images: ['img/carolus-rex.jpg','img/coat-of-arms.jpg', 'img/heroes.jpg', 'img/the-great-war.jpg', 'img/the-last-stand.jpg'],
+        intervallo:''
+        
     },
 
     methods: {
         nextAlbum() {
-            
-            if (this.contatore == this.images.length - 1){
+            this.contatore++;
+            if (this.contatore > this.images.length - 1){
                 this.contatore = 0;
-            } else {this.contatore++;}
+            }
         },
         prevAlbum() {
-            
-            if (this.contatore == 0){
+            this.contatore--;
+            if (this.contatore < 0){
                 this.contatore = this.images.length - 1;
-            } else {this.contatore--;}
+            } 
         },
         change(i) {
             this.contatore = i;
-        }
+        },
+
+        play() {
+            clearInterval(this.intervallo);
+            this.intervallo = setInterval(this.nextAlbum, 3000);
+        },
+
+        pause() {
+            clearInterval(this.intervallo);
+        },
+
     },
 
     mounted() {
-        setInterval(this.nextAlbum, 3000);
+        this.intervallo = setInterval(this.nextAlbum, 3000);
     }
 });
